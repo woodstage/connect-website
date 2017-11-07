c792cf592b04d14b33cd5a12e2164b15282a3100
Jason Yin <jiangsheng.yin@hp.com>
2017-11-07 18:12:46 +0800

    integrate signaling-server-client

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,18 +1,27 @@
 import React, { PureComponent } from "react";
 import CognitoHandler from "../utils/CognitoHandler";
 import io from "socket.io-client";
+import SignalingServerClient from "signaling-server-client";
 
 class SignIn extends PureComponent {
   signIn() {
     CognitoHandler.signIn(this.username.value, this.password.value)
       .then(result => {
         console.log("access token + " + result.getAccessToken().getJwtToken());
-        const socket = io(
-          "wss://localhost:8080?token=" + result.getAccessToken().getJwtToken()
-        );
-        socket.on("user-list", message => {
-          console.log("user list: ", message);
-        });
+        const client = new SignalingServerClient();
+        // client.connect("wss://ec2-52-199-211-229.ap-northeast-1.compute.amazonaws.com:8080?token=" + result.getAccessToken().getJwtToken()).then(result => {
+        client.connect("wss://localhost:8080?token=" + result.getAccessToken().getJwtToken()).then(result => {
+          console.log(result);
+        })
+        client.on("users", data => {
+          console.log("users:", data);
+        })
+
+        client.on("message", (message) => {
+          console.log("message receiced: ", message);
+          return {"reply": "hello"};
+        })
+        window.client = client;
       })
       .catch(err => console.log(err));
   }
