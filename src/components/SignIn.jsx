c8292bc498f41c62969434a83277a3278740acde
Jason Yin <jiangsheng.yin@hp.com>
2017-11-17 10:42:03 +0800

    add online/offline

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -9,18 +9,31 @@ class SignIn extends PureComponent {
       .then(result => {
         console.log("access token + " + result.getAccessToken().getJwtToken());
         const client = new SignalingServerClient();
-        // client.connect("wss://ec2-52-199-211-229.ap-northeast-1.compute.amazonaws.com:8080?token=" + result.getAccessToken().getJwtToken()).then(result => {
-        client.connect("wss://localhost:8080?token=" + result.getAccessToken().getJwtToken()).then(result => {
-          console.log(result);
-        })
+        client
+          .connect(
+            "wss://ec2-52-199-211-229.ap-northeast-1.compute.amazonaws.com:8080?token=" +
+              result.getAccessToken().getJwtToken()
+          )
+          .then(result => {
+            // client.connect("wss://localhost:8080?token=" + result.getAccessToken().getJwtToken()).then(result => {
+            console.log(result);
+          });
         client.on("users", data => {
           console.log("users:", data);
-        })
+        });
 
-        client.on("message", (message) => {
+        client.on("user_online", data => {
+          console.log("user online:", data);
+        });
+
+        client.on("user_offline", data => {
+          console.log("user offline:", data);
+        });
+
+        client.on("message", message => {
           console.log("message receiced: ", message);
-          return {"reply": "hello"};
-        })
+          return { reply: "hello" };
+        });
         window.client = client;
       })
       .catch(err => console.log(err));
--- src/utils/CognitoHandler.js
+++ src/utils/CognitoHandler.js
@@ -69,24 +69,7 @@ CognitoHandler.signIn = (username, password) => {
     user.authenticateUser(authenticationDetails, {
       onSuccess: result => {
         resolve(result);
-
-        // //POTENTIAL: Region needs to be set if not already set previously elsewhere.
-        // AWS.config.region = "<region>";
-
-        // AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        //   IdentityPoolId: "...", // your identity pool id here
-        //   Logins: {
-        //     // Change the key below according to the specific region your user pool is in.
-        //     "cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>": result
-        //       .getIdToken()
-        //       .getJwtToken()
-        //   }
-        // });
-
-        // // Instantiate aws sdk service objects now that the credentials have been updated.
-        // // example: var s3 = new AWS.S3();
       },
-
       onFailure: function(err) {
         reject(err);
       }
